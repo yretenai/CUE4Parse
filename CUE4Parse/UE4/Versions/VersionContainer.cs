cda7a0f92044e3dc4678486eac5c5f829609fa68
Ada <ada@chronovore.dev>
2024-05-31 01:44:06 +0100

    never strip VTs on TFD

--- CUE4Parse/UE4/Versions/VersionContainer.cs
+++ CUE4Parse/UE4/Versions/VersionContainer.cs
@@ -86,7 +86,7 @@ namespace CUE4Parse.UE4.Versions
             Options["SoundWave.UseAudioStreaming"] = Game >= GAME_UE4_25 && Game != GAME_UE4_28 && Game != GAME_GTATheTrilogyDefinitiveEdition && Game != GAME_ReadyOrNot && Game != GAME_BladeAndSoul; // A lot of games use this, but some don't, which causes issues.
             Options["AnimSequence.HasCompressedRawSize"] = Game >= GAME_UE4_17; // Early 4.17 builds don't have this, and some custom engine builds don't either.
             Options["StaticMesh.HasNavCollision"] = Ver >= EUnrealEngineObjectUE4Version.STATIC_MESH_STORE_NAV_COLLISION && Game != GAME_GearsOfWar4 && Game != GAME_TEKKEN7;
-            Options["VirtualTextureBuiltData.NeverStrip"] = false;
+            Options["VirtualTextureBuiltData.NeverStrip"] = Game == GAME_TheFirstDescendant;
 
             // special general property workarounds
             Options["ByteProperty.TMap64Bit"] = false;
